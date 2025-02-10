ca86d55df41b2767929d242ba630a7c05780095d
Amit Charungoo <v-2amitc@hotmail.com>
2025-02-10 13:57:48 +0530

    workflow update

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from MyApp to you - Dev-staging-prod environment!... - v3.4.6- used a newer way to update the version tag"
+    return "Hello from MyApp to you - Dev-staging-prod environment!... - v3.4.7- used a newer way to update the version tag"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
